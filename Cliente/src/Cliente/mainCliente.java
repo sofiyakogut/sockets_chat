3ae794e3ee2592b59f4840cf249a75cab3e5808e
sofiyakogut <113117982+sofiyakogut@users.noreply.github.com>
2023-04-12 16:34:40 +0200

    Arreglando cosas

--- Cliente/src/Cliente/mainCliente.java
+++ Cliente/src/Cliente/mainCliente.java
@@ -5,39 +5,48 @@ import java.net.*;
 import java.util.Scanner;
 
 public class mainCliente {
+	public static void main(String[] args) {
+        String host = "localhost";
+        int puerto = 1234;
+        String palabraClave = "chao";
+
+        try {
+        	Socket socket = new Socket(host, puerto);
+        	PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+        	BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        	Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Iniciando cliente... OK");
+
+            String mensaje;
+            boolean continuar = true;
+
+            while (continuar) {
+                System.out.print("Cliente: ");
+                mensaje = scanner.nextLine();
+                salida.println(mensaje);
+                if (mensaje.equalsIgnoreCase(palabraClave)) {
+                    continuar = false;
+                } else {
+                    mensaje = entrada.readLine();
+                    if (mensaje.equalsIgnoreCase(palabraClave)) {
+                        continuar = false;
+                    } else {
+                        System.out.println("Servidor: " + mensaje);
+                    }
+                }
+            }
+            System.out.println("Cerrando cliente... OK");
+            
+            scanner.close();
+            entrada.close();
+            salida.close();
+            socket.close();
+            
+        } catch (IOException e) {
+            System.err.println("Error en el cliente: " + e.getMessage());
+        }
+        
+    }
 	
-	
-	 public static void main(String[] args) {
-	        String host = "localhost";
-	        int puerto = 1234;
-	        String palabraClave = "chao";
-
-	        try (Socket socket = new Socket(host, puerto);
-	             PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
-	             BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	             Scanner scanner = new Scanner(System.in)) {
-
-	            System.out.println("Iniciando cliente... OK");
-
-	            String mensaje;
-	            while (true) {
-	                System.out.print("Cliente: ");
-	                mensaje = scanner.nextLine();
-	                salida.println(mensaje);
-	                if (mensaje.equalsIgnoreCase(palabraClave)) {
-	                    break;
-	                }
-
-	                mensaje = entrada.readLine();
-	                if (mensaje.equalsIgnoreCase(palabraClave)) {
-	                    break;
-	                }
-	                System.out.println("Servidor: " + mensaje);
-	            }
-	            System.out.println("Cerrando cliente... OK");
-	        } catch (IOException e) {
-	            System.err.println("Error en el cliente: " + e.getMessage());
-	        }
-	    }
-
-}
+}    
--- Servidor/src/Servidor/mainServidor.java
+++ Servidor/src/Servidor/mainServidor.java
@@ -10,35 +10,45 @@ public class mainServidor {
         int puerto = 1234;
         String palabraClave = "adios";
 
-        try (ServerSocket serverSocket = new ServerSocket(puerto)) {
+        try {
+        	ServerSocket serverSocket = new ServerSocket(puerto);
             System.out.println("Iniciando servidor... OK");
 
-            try (Socket socket = serverSocket.accept();
-                 PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
-                 BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 Scanner scanner = new Scanner(System.in)) {
+            Socket socket = serverSocket.accept();
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in);
 
-                String mensaje;
-                while (true) {
-                    mensaje = entrada.readLine();
-                    if (mensaje.equalsIgnoreCase(palabraClave)) {
-                        break;
-                    }
+            String mensaje;
+            boolean continuar = true;
+
+            while (continuar) {
+                mensaje = entrada.readLine();
+                if (mensaje.equalsIgnoreCase(palabraClave)) {
+                    continuar = false;
+                } else {
                     System.out.println("Cliente: " + mensaje);
 
                     System.out.print("Servidor: ");
                     mensaje = scanner.nextLine();
                     salida.println(mensaje);
                     if (mensaje.equalsIgnoreCase(palabraClave)) {
-                        break;
+                        continuar = false;
                     }
                 }
             }
             System.out.println("Cerrando servidor... OK");
+            
+            scanner.close();
+            entrada.close();
+            salida.close();
+            socket.close();
+            serverSocket.close();
+            
         } catch (IOException e) {
             System.err.println("Error en el servidor: " + e.getMessage());
-        }
+        } 
+        
     }
-	
 	
 }
